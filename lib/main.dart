970d1ca6b84d5073dddb469d48689f402f6ad53a
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-05-09 19:35:47 -0300

    chore: organize assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter_app/screens/signin.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SignIn Form',
+      title: 'Maby Motorcycles',
       theme: ThemeData(primarySwatch: Colors.teal),
       home: SignIn(),
     );
